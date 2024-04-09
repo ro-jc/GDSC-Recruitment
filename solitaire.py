f9b3bc532ac80ea62bdcd0ed4a13cc7634290373
Rohan Jacob <rohan.rj120@gmail.com>
2024-04-09 23:52:53 +0530

    implemented shuffle and questionable PRNG

--- solitaire.py
+++ solitaire.py
@@ -1,6 +1,25 @@
 # Following https://bicyclecards.com/how-to-play/solitaire
 
 
+class Random:
+    def __init__(
+        self, seed=0, cap=1, modulus=2**32, multiplier=1664525, increment=1013904223
+    ):
+        """Taking default values from 'Numerical Recipes'"""
+        self.cap = cap
+        self.seed = seed
+        self.modulus = modulus
+        self.multiplier = multiplier
+        self.increment = increment
+
+    def randrange(self, cap):
+        """Linear congruential generator for Pseudo-Random Number generation
+        returns Z in [0, cap-1]"""
+        self.cap = cap
+        self.seed = (self.multiplier * self.seed + self.increment) % self.modulus
+        return int(self.cap * self.seed / self.modulus)
+
+
 class Card:
     def __init__(self, rank, suit, visible=False):
         self.rank = rank
@@ -13,13 +32,23 @@ class Card:
 
 class Deck:
     ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    suits = ["H", "S", "C", "D"]
+    suits = ["♥", "♠", "♣", "♦"]
 
-    def __init__(self):
+    def __init__(self, random_seed):
         self.cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
+        self.shuffle(random_seed)
 
     def deal(self):
         return self.cards.pop()
+
+    def shuffle(self, s=2130):
+        """Fischer-Yates Shuffle"""
+        r = Random(s)
+        i = 52
+        while i > 1:
+            i -= 1
+            j = r.randrange(i)
+            self.cards[j], self.cards[i] = self.cards[i], self.cards[j]
 
 
 class Tableau:
